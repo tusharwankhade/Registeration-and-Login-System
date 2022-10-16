cb4cb1e7453fb296dfdb149d472e60ab74eb2dcf
tusharwankhade <110157494+tusharwankhade@users.noreply.github.com>
2022-10-17 00:28:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'localhost:3307');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+define('DB_NAME', 'Your database name');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
